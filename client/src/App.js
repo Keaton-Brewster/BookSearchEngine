8792c938d337f31ebf255b27dcd91541124ec9cb
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-05 06:52:53 -0500

    getting react app set up so that I can start working on building components

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./Pages/Search";
+import Saved from "./Pages/Saved";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;

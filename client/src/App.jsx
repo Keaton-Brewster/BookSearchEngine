bc95a371f727e400aede9738f82db9ffc90434be
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-05 10:21:39 -0500

    setting up pipeline

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./Pages/Search";
 import Saved from "./Pages/Saved";
+import openSocket from "socket.io-client";
 import "./assets/App.css";
 
 function App() {
+  const [response, setResponse] = useState();
+
+  useEffect(() => {
+    const socket = openSocket();
+    // const socket = io();
+    socket.on("FromAPI", (data) => {
+      setResponse(data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <Router>
       <Switch>
@@ -11,6 +27,9 @@ function App() {
         <Route exact path="/search" component={Search} />
         <Route exact path="/saved" component={Saved} />
       </Switch>
+      <p>
+        It's <time dateTime={response}>{response}</time>
+      </p>
     </Router>
   );
 }

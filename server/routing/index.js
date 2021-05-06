192ad9748723386350eaecc921398b8291a09abc
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-06 02:06:42 -0500

    help

--- server/routing/index.js
+++ server/routing/index.js
@@ -5,10 +5,10 @@ const bookRoutes = require("./books");
 router.use("/api/books", bookRoutes);
 
 // If no API routes are hit, send the React app
-router.get("*", (request, response) => {
-  response.sendFile(
-    path.resolve(__dirname, "../../client/build", "index.html")
-  );
+const root = require("path").join(__dirname, "client", "build");
+router.use(express.static(root));
+router.get("*", (req, res) => {
+  res.sendFile("index.html", { root });
 });
 
 module.exports = router;
